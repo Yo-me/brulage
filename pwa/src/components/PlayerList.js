c0bdb58354e36a063af2fef86b3df7459d9268bd
Guillaume LABARTHE <guillaume.labarthe@gmail.com>
2021-06-09 19:20:48 +0200

    Put PlayerList in a separated component

---
+++ pwa/src/components/PlayerList.js
@@ -0,0 +1,162 @@
+
+import React from 'react';
+
+import { 
+    CircularProgress,
+    List,
+    ListItem,
+    Chip,
+    InputBase,
+    Box
+} from '@material-ui/core'
+import { fade, withStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+
+const axios = require('axios');
+
+const styles = theme => ({
+    grow: {
+        flexGrow: 1,
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        width: '100%',
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+    title: {
+        display: 'block'
+    },
+    listContainer: {
+        width: '100%',
+        overflowY: 'auto',
+        maxHeight: "600px",
+        border: `2px solid ${theme.palette.primary.main}`,
+        boxSizing: 'border-box',
+        borderRadius: `0px 0px ${theme.shape.borderRadius}px ${theme.shape.borderRadius}px`,
+    },
+    searchBar: {
+        width: "100%",
+        borderRadius: `${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0px 0px`,
+    }
+});
+
+class PlayerList extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            players: [],
+            isLoaded: false,
+            error: null,
+            playerFilter: ""
+        }
+        this.updateFilter = this.updateFilter.bind(this);
+    }
+
+    updateFilter(event) {
+        this.setState({
+            playerFilter: event.target.value
+        });
+    }
+
+    componentDidMount() {
+        axios.get("/players?itemsPerPage=1000").then((result) => {
+            this.setState({
+                players: result.data["hydra:member"],
+                isLoaded: true
+            });
+        }).catch((error) => {
+            this.setState({
+                error: "Error loading player list"
+            });
+        });
+    }
+
+    render()
+    {
+        let playerContent = null;
+
+        if(this.state.error)
+        {
+            <p>this.state.error</p>
+        }
+        else if(!this.state.isLoaded)
+        {
+            playerContent = <CircularProgress />;
+        }
+        else 
+        {
+            playerContent = this.state.players.filter((player) => {
+                if(this.state.playerFilter == "")
+                {
+                    return true;
+                }
+                else
+                {
+                    if(player.firstname.toLowerCase().startsWith(this.state.playerFilter.toLowerCase())
+                        || player.lastname.toLowerCase().startsWith(this.state.playerFilter.toLowerCase()))
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+            }).map((player) => {
+                return (
+                    <ListItem key={player.licenceNum}>
+                        <Chip color="primary" label={player.firstname + " " + player.lastname}/>
+                    </ListItem>)
+            });
+        }
+        return (
+            <>
+                <Box className={this.props.classes.searchBar + " MuiAppBar-colorPrimary"}>
+                    <div className={this.props.classes.search}>
+                        <div className={this.props.classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase classes={{
+                            root: this.props.classes.inputRoot,
+                            input: this.props.classes.inputInput
+                        }}
+                        placeholder="Filtrer"
+                        onChange={this.updateFilter} />
+                    </div>
+                </Box>
+                <div className={"MuiPaper-elevation2 " + this.props.classes.listContainer}>
+                    <List>
+                        {playerContent}
+                    </List>
+                </div>
+            </>);
+    }
+}
+
+export default withStyles(styles)(PlayerList);
